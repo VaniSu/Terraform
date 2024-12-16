e842416ad495ad6de342ac844d287624e3927c8b
VaniSu <vanisuresh2102@gmail.com>
2024-12-16 19:27:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,71 +12,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_instance" "instance" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "VPC"
+    Name = "HelloWorld"
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
 
   tags = {
-    Name = "PublicSubnet"
+    Name        = "My bucket"
   }
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "PrivateSubnet"
-  }
-}
-
-resource "aws_internet_gateway" "int_gw" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "InternetGateway"
-  }
-}
-
-resource "aws_route_table" "pub_rt" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.int_gw.id
-  }
-
-  tags = {
-    Name = "PubRouteTable"
-  }
-}
-
-resource "aws_route_table" "pri_rt" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "PriRouteTable"
-  }
-}
-
-
-resource "aws_route_table_association" "public_sb_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.pub_rt.id
-}
-
-resource "aws_route_table_association" "pri_sb_assoc" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.pri_rt.id
 }
 
 
