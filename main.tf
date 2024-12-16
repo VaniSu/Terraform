63bc51f8e53b005596c589beaee67a0b8ad52121
VaniSu <vanisuresh2102@gmail.com>
2024-12-16 19:02:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,11 +63,6 @@ resource "aws_route_table" "pub_rt" {
 resource "aws_route_table" "pri_rt" {
   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
-  }
-
   tags = {
     Name = "PriRouteTable"
   }
