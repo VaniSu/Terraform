d58607a5e09c06f8cc1270b0c9ae0074231a8826
VaniSu <vanisuresh2102@gmail.com>
2024-12-16 19:40:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,17 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
+  alias = "eu-central-1"
   region = "eu-central-1"
 }
 
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+}
+
 resource "aws_instance" "instance" {
+  provider      = aws.us-east-1
   ami           = "ami-0453ec754f44f9a4a"
   instance_type = "t2.micro"
 
@@ -22,7 +29,8 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "bucket6718787hgv"
+  provider = aws.eu-central-1
+  bucket   = "bucket6718787hgv"
 
   tags = {
     Name        = "My bucket"
