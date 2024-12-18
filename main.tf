9cac4cefa0e26a84cc346c3dddb56390c1a5039a
VaniSu <vanisuresh2102@gmail.com>
2024-12-18 14:07:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,53 +1,37 @@
 terraform {
+  backend "s3" {
+    bucket         = "bucket6718787hgv"
+    key            = "networking/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
   }
 }
-
-# Configure the AWS Provider
+ 
 provider "aws" {
-  alias = "eu-central-1"
-  region = "eu-central-1"
+  region = "us-east-1"
 }
 
-
-resource "aws_s3_bucket" "terraform_state" {
-  provider = aws.eu-central-1
-  bucket   = "bucket6718787hgv"
-  acl    = "private"
+module "backend" {
+  source      = "https://github.com/VaniSu/Terraform/tree/main/backend"
  
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = "TerraformStateBucket"
-    Environment = "Infrastructure"
-  }
 }
  
-# DynamoDB Table for State Locking
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock-table"
-  provider = aws.eu-central-1
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
- 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
- 
-  tags = {
-    Name        = "TerraformStateLockTable"
-    Environment = "Infrastructure"
-  }
+# Module A
+module "Compute" {
+  source      = "https://github.com/VaniSu/Terraform/tree/main/compute"
+  
 }
-
-
-
-
+ 
+# Module B
+module "network" {
+  source      = "https://github.com/VaniSu/Terraform/tree/main/networking"
+ 
+}
 
