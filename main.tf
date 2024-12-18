aa1d3764bb891cbd5a02077fed121f949d95756a
VaniSu <vanisuresh2102@gmail.com>
2024-12-18 12:37:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,5 +37,48 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  provider = aws.eu-central-1
+  bucket   = "bucket6718787hgv"
+  acl    = "private"
+ 
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "TerraformStateBucket"
+    Environment = "Infrastructure"
+  }
+}
+ 
+# DynamoDB Table for State Locking
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock-table"
+  provider = aws.eu-central-1
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags = {
+    Name        = "TerraformStateLockTable"
+    Environment = "Infrastructure"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "bucket6718787hgv"
+    key            = "compute/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
 
 
