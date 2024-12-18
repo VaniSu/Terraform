73cc4ea888263d06cc91142e035101e08b2a068e
VaniSu <vanisuresh2102@gmail.com>
2024-12-18 12:38:58 +0530

    Create main.tf

---
+++ compute/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instance" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
