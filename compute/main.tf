d109afd638065a360e3031266c14afb9b3de1b8a
VaniSu <vanisuresh2102@gmail.com>
2024-12-18 13:52:24 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -1,23 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket         = "bucket6718787hgv"
-    key            = "compute/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
 
 resource "aws_instance" "instance" {
   ami           = "ami-0453ec754f44f9a4a"
