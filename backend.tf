77606bed72f9196ec0fb4487b6e8ec1fb9fcdc7c
VaniSu <vanisuresh2102@gmail.com>
2024-12-19 20:05:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "bucket6718787hgv"
-    key            = "compute/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
+
